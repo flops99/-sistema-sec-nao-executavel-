58126c7347741870c2f1fe3f6fd08f8a80052be3
flops99 <141242543+flops99@users.noreply.github.com>
2024-11-10 01:52:29 -0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,19 +1,4 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-# Cria dados para o eixo x
-x = np.linspace(0, 2 * np.pi, 100)
-
-# Calcula os valores da função seno para cada ponto em x
-y = np.sin(x)
-
-# Cria o gráfico
-plt.plot(x, y, label='Função Seno')
-plt.title('Gráfico da Função Seno')
-plt.xlabel('Eixo X')
-plt.ylabel('Eixo Y')
-plt.legend()
-plt.grid(True)
-
-# Exibe o gráfico
-plt.show()
+a = input("insiraa a senha")
+if a == "1234":
+  print("acesso permitido")
+  print("l
